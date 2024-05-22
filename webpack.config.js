81d271de2805db07a8cd77880f51d618b3228462
biodiscus <biodiscus@gmail.com>
2024-05-22 11:49:13 +0200

    chore: update packages and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,20 @@
-const fs = require('fs');
 const path = require('path');
-const glob = require('glob-all');
 const webpack = require('webpack');
-const { merge } = require('webpack-merge');
 const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
-const Handlebars = require('handlebars');
-
-const PATHS = {
-  pages: path.join(__dirname, 'src/views/pages/'),
-  partials: path.join(__dirname, 'src/views/partials/'),
-};
-
-// Handlebars helpers used in templates.
-const handlebarsHelpers =  {
-  /**
-   * Include the partial file into a template.
-   * @param {string} filename The partial file name.
-   * @param {Object} options The options passed via tag attributes into a template.
-   * @param {Object} args The parent options passed using the `this` attribute.
-   * @return {Handlebars.SafeString}
-   */
-  include: (filename, options, args) => {
-    const tmplExt = '.html';
-    const { ext } = path.parse(filename);
-    if (!ext) filename += tmplExt;
-    const template = fs.readFileSync(`${PATHS.partials}${filename}`, 'utf8');
-    // pass the original data into sub-sub partials
-    const data = options.name === 'include' ? {...options?.hash, ...options.data?.root} : {...options, ...args?.data?.root};
-    const html = Handlebars.compile(template)(data);
-    return new Handlebars.SafeString(html);
-  },
-  limit: (arr, limit) => {
-    if (!Array.isArray(arr)) { return []; }
-    return arr.slice(0, limit);
-  }
-};
-
-// Register handlebars helpers.
-for (const helper in handlebarsHelpers) {
-  Handlebars.registerHelper(helper, handlebarsHelpers[helper]);
-}
 
 // Project config data.
 // Go here to change stuff for the whole demo, ie, change the navbar.
 // Also go here for the various data loops, ie, category products, slideshows.
-const projectData = merge(
-    { webRoot: '' },
-    {config: require(path.join(__dirname, 'src/data/config.json')) },
-  { 'category-products': require(path.join(__dirname, 'src/data/category-products.json')) },
-  { 'cart-items': require(path.join(__dirname, 'src/data/cart-items.json')) },
-  { 'filters-one': require(path.join(__dirname, 'src/data/filters-one.json')) },
-  { 'options-size-one': require(path.join(__dirname, 'src/data/options-size-one.json')) },
-  { 'options-size-two': require(path.join(__dirname, 'src/data/options-size-two.json')) },
-  { 'slideshow-brands-one': require(path.join(__dirname, 'src/data/slideshow-brands-one.json')) },
-);
-
-// Create the entry object containing pages located in src/views/pages/ directory.
-const entry = glob.sync(path.join(PATHS.pages, '/**/*.html')).reduce((entry, file) => {
-  const name = path.relative(PATHS.pages, file).replace(/\.html$/, '');
-  entry[name] = file;
-  return entry;
-}, {});
+const projectData = {
+  webRoot: '',
+  config: require(path.join(__dirname, 'src/data/config.json')),
+  'category-products': require(path.join(__dirname, 'src/data/category-products.json')),
+  'cart-items': require(path.join(__dirname, 'src/data/cart-items.json')),
+  'filters-one': require(path.join(__dirname, 'src/data/filters-one.json')),
+  'options-size-one': require(path.join(__dirname, 'src/data/options-size-one.json')),
+  'options-size-two': require(path.join(__dirname, 'src/data/options-size-two.json')),
+  'slideshow-brands-one': require(path.join(__dirname, 'src/data/slideshow-brands-one.json')),
+};
 
 // Main webpack config options.
 module.exports = (env, argv) => {
@@ -88,10 +42,10 @@ module.exports = (env, argv) => {
       new webpack.ProgressPlugin(),
 
       new HtmlBundlerPlugin({
-        // automatically detect pages in ./src/views/pages/ folder
-        entry,
+        // automatically detect pages located in the directory
+        entry: 'src/views/pages/',
 
-        // you can manually define pages
+        // -OR- you can define pages manually
         // entry: {
         //   index: './src/views/pages/index.html',
         //   category: './src/views/pages/category.html',
@@ -102,16 +56,31 @@ module.exports = (env, argv) => {
         // },
 
         js: {
-          // output filename of extracted JS
+          // JS output filename
           filename: 'assets/js/[name].[contenthash:8].js',
         },
         css: {
-          // output filename of extracted CSS
+          // CSS output filename
           filename: 'assets/css/[name].[contenthash:8].css',
         },
-
-        loaderOptions: {
-          preprocessor: (content) => Handlebars.compile(content)(projectData),
+        // external data used in templates
+        data: projectData,
+        // use Handlebars templating engine to render pages
+        preprocessor: 'handlebars',
+        preprocessorOptions: {
+          views: [
+            path.join(__dirname, 'src/views/partials/'),
+          ],
+          partials: [
+            path.join(__dirname, 'src/views/partials/'),
+          ],
+          // custom handlebars helpers used in templates
+          helpers: {
+            limit: (arr, limit) => {
+              if (!Array.isArray(arr)) { return []; }
+              return arr.slice(0, limit);
+            },
+          },
         },
       }),
     ],
@@ -121,7 +90,6 @@ module.exports = (env, argv) => {
         {
           test: /\.(sass|scss|css)$/,
           use: ['css-loader', 'postcss-loader', 'sass-loader'],
-          //use: ['css-loader', 'sass-loader'],
         },
         {
           test: /[\\/]images[\\/].+(png|jpe?g|webp|ico|svg)$/i,
@@ -144,8 +112,8 @@ module.exports = (env, argv) => {
       splitChunks: {
         cacheGroups: {
           vendor: {
-            test:   /[\\/](node_modules)[\\/].+\.js$/,
-            name:   'vendor',
+            test: /[\\/](node_modules)[\\/].+\.js$/,
+            name: 'vendor',
             chunks: 'all'
           }
         }
@@ -156,11 +124,11 @@ module.exports = (env, argv) => {
       hints: false,
     },
 
-    // enable HMR with live reload
     devServer: {
       static: {
         directory: path.join(__dirname, 'dist'),
       },
+      // enable live reload
       watchFiles: {
         paths: ['src/**/*.*'],
         options: {
